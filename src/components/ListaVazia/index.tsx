d85af8d892d805ac92e1b3024f115a2da438eb3d
Guilherme Padilha <gs.padilha@gmail.com>
2023-05-11 06:37:51 -0300

    fix: alteração das imagens para a pasta public

--- src/components/ListaVazia/index.tsx
+++ src/components/ListaVazia/index.tsx
@@ -3,7 +3,7 @@ import style from './style.module.css';
 export function ListaVazia() {
   return (
     <section aria-label='lista-vazia' className={style.wrapperListaVazia}>
-      <img src='./src/assets/images/clipboard.png' />
+      <img src='/assets/images/clipboard.png' />
       <strong>Você ainda não tem tarefas cadastradas</strong>
       <span>Crie tarifas e organize seus itens a fazer</span>
     </section>
